5e387618541e873b1410000d5a39d09ed0c8053e
MinRi2 <2275045670@qq.com>
2025-04-20 11:57:45 +0800

    Update script. Create BLACKLIST

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,59 @@ import { readdir, readFile, rm } from "fs/promises";
 import path from "path";
 import XLSX from "node-xlsx";
 import { startPolling } from "./utils";
+import { existsSync } from "fs";
 
-const OUT_PATH = path.resolve("./schematics");
-const SCHEMATIC_SUFFIX = ".msch";
+import blacklist from "../blacklist.json";
 
-const QQ_DOC_COOKIES = Bun.env["QQ_DOC_COOKIES"]!;
+class SchematicDataMap extends Map<string, SchematicData> {
+    public static getKey(category: string, name: string) {
+        return category + name;
+    }
 
-const DOC_ID = "300000000$TshKyHrmMlQR";
-const WISE_BOOK = "智能表1";
+    public static getKeyMeta(meta: SchematicMeta) {
+        return SchematicDataMap.getKey(meta.category, meta.name);
+    }
+
+    setData(param: SchematicData | SchematicData[]){
+        if(param instanceof Array){
+            if(param.length) param.forEach(meta => this.set(SchematicDataMap.getKeyMeta(meta), meta));
+        }else if(param instanceof SchematicData){
+            this.set(SchematicDataMap.getKeyMeta(param), param);
+        }
+    }
+
+    getData(category: string, name: string) {
+        return this.get(SchematicDataMap.getKey(category, name));
+    }
+
+    hasData(category: string, name: string) {
+        return this.has(SchematicDataMap.getKey(category, name));
+    }
+
+    deleteData(data: SchematicMeta) {
+        return this.delete(SchematicDataMap.getKey(data.category, data.name));
+    }
+}
+
+class SchematicData implements SchematicMeta {
+    public static SCHEMATIC_SUFFIX = ".msch";
+
+    constructor(public category: string, public name: string, public base64: string) {
+        this.name = name.replaceAll("/", "-").replaceAll("\n", "-");
+    }
+
+    getFileName() {
+        return this.name + SchematicData.SCHEMATIC_SUFFIX;
+    }
+
+    getFilePath(basePath: string) {
+        return path.resolve(basePath, this.category, this.getFileName());
+    }
+
+    equals(other: SchematicMeta) { 
+        return this.category === other.category && this.name === other.name && this.base64 === other.base64;
+    }
+}
 
 interface ExportData {
     ret: number;
@@ -25,49 +70,54 @@ interface QueryData {
     file_size?: number;
 }
 
-interface SchematicData {
+interface ContextData{
+    qqCookies: string;
+    outPath: string;
+    lastData: SchematicDataMap;
+    data: SchematicDataMap;
+}
+
+interface SchematicMeta{
     category: string;
     name: string;
     base64: string;
 }
 
+const context: ContextData = {
+    outPath: "./schematics",
+    qqCookies: Bun.env["QQ_DOC_COOKIES"]!,
+    lastData: new SchematicDataMap(),
+    data: new SchematicDataMap(),
+}
+
+const DOC_ID = "300000000$TshKyHrmMlQR";
+const WISE_BOOK = "智能表1";
+
 run();
 
 async function run() {
-    let buffer: Buffer;
+    context.outPath = path.resolve(context.outPath);
 
-    if(Bun.env.NODE_ENV === "local"){
-        buffer = await readFile(path.resolve("Mindustry 蓝图档案馆.xlsx"));
-    }else{
-        if (!QQ_DOC_COOKIES || QQ_DOC_COOKIES == "") {
-            console.error("No QQ_DOC_COOKIES!");
-            return;
-        }
+    const { outPath, lastData, data } = context;
+    lastData.setData(await readData(context.outPath));
 
-        try {
-            const arrayBuffer = await fetchSchematicsExcel();
-            buffer = Buffer.from(arrayBuffer);
-        } catch (error) {
-            console.error(error);
-            console.error("Failed to fetch schematics excel. Please check your QQ_DOC_COOKIES.");
-            return;
-        }
+    const excelData = await readExcel();
+    if(excelData === undefined){
+        console.error("Failed to parse excel data.");
+        return;
     }
 
-    await rm(OUT_PATH, {
-        recursive: true,
-        force: true
-    });
+    data.setData(await parseExcelData(excelData[0]!.data));
 
-    const data = await handleExcel(buffer);
-    await genSchematics(data);
+    await genSchematics(outPath, data, lastData);
+    await cleanSchematics(outPath, data, lastData);
 }
 
 async function fetchSchematicsExcel() {
     let resp = await fetch("https://docs.qq.com/v1/export/export_office", {
         "headers": {
             "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "cookie": QQ_DOC_COOKIES,
+            "cookie": context.qqCookies,
         },
         "body": `exportType=0&switches=%7B%22embedFonts%22%3Afalse%7D&docId=${DOC_ID}`,
         "method": "POST",
@@ -82,7 +132,7 @@ async function fetchSchematicsExcel() {
     const data = await startPolling(async () => {
         const resp = await fetch(`https://docs.qq.com/v1/export/query_progress?operationId=${operationId}`, {
             "headers": {
-                "cookie": QQ_DOC_COOKIES,
+                "cookie": context.qqCookies,
             },
             "method": "GET"
         });
@@ -103,19 +153,51 @@ async function fetchSchematicsExcel() {
     return await resp.arrayBuffer();
 }
 
-async function handleExcel(buffer: Buffer) {
+async function readExcel() {
+    let buffer: Buffer;
+    if(Bun.env.NODE_ENV === "local"){
+        buffer = await readFile(path.resolve("Mindustry 蓝图档案馆.xlsx"));
+    }else{
+        if (!context.qqCookies || context.qqCookies == "") {
+            console.error("No QQ_DOC_COOKIES!");
+            return;
+        }
+
+        try {
+            const arrayBuffer = await fetchSchematicsExcel();
+            buffer = Buffer.from(arrayBuffer);
+        } catch (error) {
+            console.error(error);
+            console.error("Failed to fetch schematics excel. Please check your QQ_DOC_COOKIES.");
+            return;
+        }
+    }
+
     const excelData = XLSX.parse(buffer, {
         type: "buffer",
         sheets: WISE_BOOK,
         cellHTML: false,
     });
 
-    const schematicsData: SchematicData[] = excelData[0]!.data.filter(arr => {
+    return excelData;
+}
+
+async function parseExcelData(excelData: any[]) {
+    const schematicsData: SchematicData[] = excelData.filter((arr, index) => {
+        if(index === 0) return false; // Skip header row
+
         const [category, author, name, _, base64] = arr;
+
+        // schematics causing buffer mismatch will be listed.
+        if(blacklist.schematics.findIndex(s => s === name) !== -1 
+        || blacklist.authors.findIndex(a => a === author) !== -1){
+            return false;
+        }
+
         return isValidBase64(base64) || console.error("Invalid schematic", name);
     }).map(arr => {
         const [category, author, name, _, base64] = arr;
-        return { category, name, base64 };
+        return new SchematicData(category, name, base64);
     });
 
     return schematicsData;
@@ -125,36 +207,63 @@ async function handleExcel(buffer: Buffer) {
     }
 }
 
-async function genSchematics(schematicsData: SchematicData[]) {
-    const jobs = schematicsData.map(async (data) => {
-        const { category, name, base64 } = data;
-
-        const handledName = name.replaceAll("/", "-").replaceAll("\n", "-");
-        const fileName = handledName + SCHEMATIC_SUFFIX;
-        const filePath = path.join(OUT_PATH, category, fileName);
-
-        try {
-            await Bun.write(filePath, Buffer.from(base64, "base64"));
-            console.log("Save schematic", fileName);
-        } catch (error) {
-            console.error("Failed to save", filePath, error);
+async function genSchematics(fromPath: string, dataMap: SchematicDataMap, lastDataMap: SchematicDataMap) {
+    let count = 0;
+    const jobs = Array.from(dataMap.values()).map(async (data) => {
+        if(lastDataMap.getData(data.category, data.name)?.equals(data)){
+            return;
         }
+        const { base64 } = data;
+        const fileName = data.getFileName();
+        const filePath = data.getFilePath(fromPath);
+        
+        const buffer = Buffer.from(base64, "base64");
+        const file = Bun.file(filePath);
+
+        await Bun.write(file, buffer);
+
+        count++;
+        console.log("Save schematic", fileName);
     });
 
     await Promise.all(jobs);
 
-    console.log("All schematics saved to", OUT_PATH);
-    console.log("Total schematics:", schematicsData.length);
+    console.log("All schematics saved to", context.outPath);
+    console.log("Total schematics:", dataMap.size);
+    console.log("Saved schematics:", count);
 }
 
-async function readData() {
+async function cleanSchematics(fromPath: string, dataMap: SchematicDataMap, lastDataMap: SchematicDataMap) {
+    const jobs = Array.from(lastDataMap.entries()).map(async entry => {
+        const [key, item] = entry;
+
+        if(dataMap.has(key)) {
+            return;
+        }
+
+        const filePath = item.getFilePath(fromPath);
+
+        try {
+            await rm(filePath, { force: true, recursive: true });
+            console.log("Remove schematic", filePath);
+        } catch (error) {
+            console.error("Failed to remove schematic", filePath, error);
+        }
+    });
+
+    await Promise.all(jobs);
+}
+
+async function readData(fromPath: string) {
     const schematicsData: SchematicData[] = [];
 
-    const fileNames = await readdir(path.resolve(OUT_PATH));
-    const readDataJobs = fileNames.map(async (category) => {
-        console.log("Read category:", category);
+    if(!existsSync(fromPath)){
+        return [];
+    }
 
-        const categoryPath = path.join(OUT_PATH, category);
+    const fileNames = await readdir(fromPath);
+    const readDataJobs = fileNames.map(async (category) => {
+        const categoryPath = path.join(fromPath, category);
         const schematicFiles = await readdir(categoryPath);
 
         const readJobs = schematicFiles.map(async (schematicFileName) => {
@@ -164,8 +273,8 @@ async function readData() {
                 const buffer = Buffer.from(await Bun.file(schematicFilePath).arrayBuffer());
                 const base64 = buffer.toBase64();
                 
-                schematicsData.push({ category, name: path.basename(schematicFileName, SCHEMATIC_SUFFIX), base64 });
-                console.log("Read schematic", schematicFileName);
+                const data = new SchematicData(category, path.basename(schematicFileName, SchematicData.SCHEMATIC_SUFFIX), base64)
+                schematicsData.push(data);
             } catch (error) {
                 console.error("Failed to read schematic:", schematicFilePath, error);
             }
@@ -175,6 +284,8 @@ async function readData() {
     });
 
     await Promise.all(readDataJobs);
+
+    console.log("Read schematics data:", schematicsData.length);
 
     return schematicsData;
 }
